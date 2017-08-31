71fd190b6e7cf7b71c6d351728cb203fd07181fd
Khai Bui <khaivubui@gmail.com>
2017-08-30 22:51:23 -0700

    Actually finish phase III, so happy

--- w5d4/phase2.js
+++ w5d4/phase2.js
@@ -20,3 +20,29 @@ Elephant.prototype.play = function(trick) {
   let index = Math.round(Math.random() * (this.tricksArr.length - 1));
   console.log(`${this.name} is ${this.tricksArr[index]}`);
 };
+
+let ellie = new Elephant("Ellie", 185, [
+  "giving human friends a ride",
+  "playing hide and seek"
+]);
+let charlie = new Elephant("Charlie", 200, [
+  "painting pictures",
+  "spraying water for a slip and slide"
+]);
+let kate = new Elephant("Kate", 234, [
+  "writing letters",
+  "stealing peanuts"
+]);
+let micah = new Elephant("Micah", 143, [
+  "trotting",
+  "playing tic tac toe",
+  "doing elephant ballet"
+]);
+
+let herd = [ellie, charlie, kate, micah];
+
+let paradeHelper = (elephant) => {
+  console.log(`${elephant.name} is trotting by!`);
+};
+
+herd.forEach(paradeHelper);
