0e1fa56bb150a1a3b6c4af60a421672eecfeeb6f
Khai Bui <khaivubui@gmail.com>
2017-09-12 23:36:59 -0700

    Complete hw;

--- w7d3/giphy/components/giphys_search.jsx
+++ w7d3/giphy/components/giphys_search.jsx
@@ -1,3 +1,22 @@
 import React from 'react';
 
 import GiphysIndex from './giphys_index';
+
+const GiphysSearch = (props) => (
+  <div>
+    <form onSubmit={e => {
+        e.preventDefault();
+        props.fetchSearchGiphys(
+          e.target.querySelector('input[type=text]').value
+        );
+      }}>
+      <input type='text' name='query'/>
+      <input type='submit' value='Search'/>
+    </form>
+    {props.giphys.map(giphy => (
+      <img key={giphy.id} src={giphy.images.fixed_height.url}/>
+    ))}
+  </div>
+);
+
+export default GiphysSearch;
--- w7d3/giphy/giphy_search.jsx
+++ w7d3/giphy/giphy_search.jsx
@@ -2,3 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+
+import { fetchSearchGiphys } from './actions/giphy_actions.js';
+
+window.fetchSearchGiphys = fetchSearchGiphys;
+
+const store = configureStore();
+
+document.addEventListener('DOMContentLoaded', () => {
+  window.store = store;
+  ReactDOM.render(
+    <Root store={store}/>,
+    document.getElementById('root')
+  );
+});
